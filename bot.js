64fe6452f0253036a99c100e5e52fa1d4cd8305a
mphvictrsepng-rgb <mphvoctrsepng@gmail.com>
2026-06-27 12:56:17 +0200

    first commit

---
+++ bot.js
@@ -0,0 +1,32 @@
+console.log('Hi there!');                     //prinout to the user, no need to make a variable to store this info
+console.log('I am excited to talk to you.');
+
+let bot;
+bot = 'teacherBot';
+
+let botLocation = 'the universe';
+
+console.log('Allow me to introduce myself.');
+
+const botIntroduction = 'My name is ' + bot + '.';
+console.log(botIntroduction);
+
+const botLocationSentence = 'I live in ' + botLocation + '.';
+console.log(botLocationSentence);
+
+bot = 'professorBot';     //reassigned a new value to the bot variable
+
+const nicknameIntroduction = 'My nickname is ' + bot + '.';
+console.log(nicknameIntroduction);
+
+bot = 'awesomeTeacherBot';
+
+const newNicknameGreeting = 'I love my nickname but I wish people would call me ' + bot + '.';
+console.log(newNicknameGreeting);
+
+const favoriteSubject = 'Computer Science';
+
+const favoriteSubjectSentence = 'My favorite subject is ' + favoriteSubject + '.';
+console.log(favoriteSubjectSentence);
+
+console.log('Well, it was nice to talk to you. Have an awesome day!')
